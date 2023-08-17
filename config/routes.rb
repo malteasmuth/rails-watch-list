ea5bd987e2b6a4530ed715a0f0a776ed4a8898e6
Malte Asmuth <malte.olaf.asmuth@web.de>
2023-08-17 14:19:45 +0200

    Finish work on db

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/new'
+  get 'lists/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'lists#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+url = "http://tmdb.lewagon.com/movie/top_rated?"
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)
+
+Movie.destroy_all
+
+movies["results"].each do | movie |
+
+  poster_url = movie["poster_path"]
+
+  new_movie = Movie.new(
+    title: movie["original_title"],
+    overview: movie["overview"],
+    poster_url: "https://image.tmdb.org/t/p/original#{poster_url}"
+  )
+  new_movie.save
+end
